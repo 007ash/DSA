1b3644f43888b4763d89bbeb695f117156817137
007ash <6ashwanth007@gmail.com>
2025-07-29 19:16:50 +0530

    2d array

--- array.py
+++ array.py
@@ -1,8 +1,18 @@
-n = 8
-for i in range(n):
-    for j in range(n):
-        if  i == j or i+j == n-1:
-            print("*", end=" ")
-        else:
-            print(" ",end=" ")
-    print()
+def access(matrix, row, col):
+    if row > len(matrix[0])-1 or col > len(matrix)-1:
+        return "Invalid Row and column"
+    
+    return matrix[row][col]
+
+def search(matrix, val):
+    row = len(matrix[0])
+    col = len(matrix)
+    for r in range(row):
+        for c in range(col):
+            if matrix[r][c] == val:
+                return (r+1,c+1)
+    return -1
+
+
+mat = [[1,2,3],[4,5,6],[7,8,9]]
+print(search(mat,9))
