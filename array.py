413bf362dcfafb5b1df7970af897147f9f3df504
007ash <6ashwanth007@gmail.com>
2025-07-30 19:19:19 +0530

    rotate 90 p1

--- array.py
+++ array.py
@@ -14,19 +14,7 @@ def search(matrix, val):
     return -1
 
 def rotate_90(matrix):
-    rot = []
-    rotate_matrix = []
-    row = len(matrix[0])
-    col = len(matrix)
-    for r in range(row):
-        for c in range(col,0,-1):
-            rot.extend(r[c])
-        rotate_matrix.append(rot)
-    
-    return rotate_matrix
-        
-
-
+    pass
 
 mat = [[1,2,3],[4,5,6],[7,8,9]]
 print(rotate_90(mat))
